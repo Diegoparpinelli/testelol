cb7f4a2815c32da8f24ad7c04433cf475b2da628
Diego Parpinelli de Paula Oliveira <diego_parpinelli@hotmail.com>
2023-02-27 23:49:49 -0400

    adsad

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require("express")
 const cors = require("cors")
 
@@ -10,17 +10,5 @@ app.use(cors({origin:"0.0.0.0"}))
 
 app.use("/", require("./routes/router"));
 
-=======
-const express = require("express")
-const cors = require("cors")
 
-const app = express()
-
-
-app.use(express.json());
-app.use(cors({origin:"0.0.0.0"}))
-
-app.use("/", require("./routes/router"));
-
->>>>>>> aef977a0048d49c26753232930131d7953035849
 app.listen(3005, ()=>console.log("sever is runing in port 3005"));
